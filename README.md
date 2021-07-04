ffa939323426045e0dc7714b05137d9c04791fac
Roman Guseynov <hidagara@gmail.com>
2021-07-04 19:41:06 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RGNetwork
+
+A description of this package.
---
+++ Tests/RGNetworkTests/RGNetworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RGNetwork
+
+final class RGNetworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RGNetwork().text, "Hello, World!")
+    }
+}
